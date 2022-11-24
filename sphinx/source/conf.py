653dcfcd94cce35dc92204d1ce3c39ebb708e9ef
sample-nick <victormaiacouto02@gmail.com>
2022-11-24 18:45:33 -0300

    Added a new theme for the documetation's website

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -1,3 +1,5 @@
+import sphinx_rtd_file
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -14,7 +16,7 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx_rtd_theme']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +26,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
